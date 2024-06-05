5b5fd858128ed608fa792153463076b60082f1a0
Lucas Nóbrega de Araújo <159348877+lucrasn@users.noreply.github.com>
2024-06-05 23:37:12 +0000

    Documentação do arquvo main.py quase completo Changes to be committed: 	modified:   projeto/main.py

--- projeto/main.py
+++ projeto/main.py
@@ -2,39 +2,47 @@ import cv2
 import mediapipe as mp
 from package import olhos_fechados as of
 
-# Inicializar MediaPipe Face Mesh
-# variável para facilitar as diversas chamadas a esse módulo
-mp_face_mesh = mp.solutions.face_mesh # atribuindo o módulo 'fase_mesh' do mediapipe à esta variável
-# outra variável de mesmo intuito mas já chamando a função 'FaceMesh()'
-    # mudando os parâmetros da função 'FaceMesh()' de confiança mínima de detecção e a de confiança mínima de rastreamento para 60%
+# Inicializar MediaPipe Face Mesh;
+# variável para facilitar as diversas chamadas a esse módulo;
+mp_face_mesh = mp.solutions.face_mesh # atribuindo o módulo 'fase_mesh' do mediapipe à esta variável;
+# outra variável de mesmo intuito mas já chamando a função 'FaceMesh()' para detectar a malha facial;
+    # mudando os parâmetros da função 'FaceMesh()' de confiança mínima de detecção e a de confiança mínima de rastreamento para 60%;
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.6, min_tracking_confidence=0.6)
 
-# Inicializar desenho de utilidades MediaPipe
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+# Inicializar desenho de utilidades MediaPipe;
+# Idem ao anterior
+mp_drawing = mp.solutions.drawing_utils # contém as funções para desenhar os pontos da malha facial;
+mp_drawing_styles = mp.solutions.drawing_styles # contém os estilos predefinidos para desenhar as conexões da malha facial;
 
 # Capturar vídeo da câmera
-webcam = cv2.VideoCapture(0)
+# Essa variavel será um objeto da classe 'cv2.VideoCapture';
+webcam = cv2.VideoCapture(0) # O parâmetro '0' refere-se a camera padrão do dispositivo;
 
-# Verifique se a câmera foi aberta corretamente
-if not webcam.isOpened():
-    print("Erro ao abrir a câmera.")
-    exit()
-
+# Controle do nosso While;
 flag = False
 
-while (webcam.isOpened()) and (not flag):
+while (webcam.isOpened()) and (not flag): # se a webcam estiver aberta e a flag for False entramos no loop;
+    # barreira de verificação:
+        # captura um frame da webcam;
+            # 'success' é um valor booleano que indica se a captura foi bem-sucedida;
+            # 'image' contém o frame capturado;
     success, image = webcam.read()
-    if not success:
+    if not success: # se a captura não foi bem-sucedida;
         print("Ignorando frame vazio da câmera.")
-        continue
+        continue # pulamos para o próximo ciclo do loop, não iremos fazer a analise de um frame que não existe;
 
-    # Converter a imagem BGR para RGB
+    # Converter a imagem BGR (Blue-Green-Red) para RGB (Red-Green_Blue);
+        # O padrão do OpenCV é o BGR mas precisamos do RGB para o MediaPipe;
+    # Conversão de espaço de cores na imagem;
+        # 'image' -> variável que contém a imagem que será convertida;
+        # 'cv2.COLOR_BGR2RGB' -> parâmetro que especifica o tipo de conversão;
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Definindo o frame como não editável para melhor desempenho durante o precessamento;
     image.flags.writeable = False
+    # Processando o frame como o modelo de malha facial do MediaPipe e guardando na variável;
     results = face_mesh.process(image)
 
-    # Converter de volta para BGR
+    # Deixando o frame editavel e convertendo de volta para BGR
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
